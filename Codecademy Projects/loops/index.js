f2df54488b4d90d3877d719f7dcfaf997049cfc6
jager <mshalewski@tutanota.com>
2023-02-21 16:43:36 -0600

    finished loops lesson

---
+++ Codecademy Projects/loops/index.js
@@ -0,0 +1,82 @@
+// let vacationSpots = ['Munich', 'Tokyo', 'Warsaw'];
+
+// console.log(vacationSpots[0]);
+// console.log(vacationSpots[1]);
+// console.log(vacationSpots[2]);
+
+// for(let i = 5; i < 11; i++){
+//     console.log(i);
+// }
+
+// The loop below loops from 0 to 3. Edit it to loop backwards from 3 to 0
+// for (let counter = 3; counter >= 0; counter--){
+//     console.log(counter);
+//   }
+
+// const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+// // Write your code below
+// for (let i = 0; i < vacationSpots.length; i++){
+//   console.log(`I would love to visit ${vacationSpots[i]}`);
+// }
+
+
+// let bobsFollowers = ['Lily', 'Tom', 'Joe', 'Jon'];
+// let tinasFollowers = ['Lily','Tom','Jake'];
+
+// let mutualFollowers = [];
+
+// for(let i = 0; i < bobsFollowers.length; i++){
+//     for(let s = 0; s < tinasFollowers.length; s++){
+//         bobsFollowers[i] === tinasFollowers[s] ? mutualFollowers.push(bobsFollowers[i]) : console.log('Bob and Tina do not share that follower!');
+//     }
+// }
+// console.log(mutualFollowers);
+
+// const cards = ['diamond', 'spade', 'heart', 'club'];
+
+// // Write your code below
+
+// let currentCard;
+
+// while(currentCard !== 'spade'){
+//   currentCard = cards[Math.floor(Math.random() * 4)];
+//   console.log(currentCard);
+// }
+
+// const cupsOfSugarNeeded = 5;
+// let cupsAdded = 0;
+// do {
+//     cupsAdded++;
+//     console.log(cupsAdded);
+// } while (cupsAdded < cupsOfSugarNeeded);
+
+// const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac", "Lil Darkie"];
+
+// // Write your code below
+
+// for (let i = 0; i < rapperArray.length; i++){
+//     if(rapperArray[i] === 'Notorious B.I.G.'){
+//         console.log(rapperArray[i]);
+//         break;
+//     }else{
+//         console.log(rapperArray[i]);
+//         console.log('And if you don\'t know, now you know.');
+
+//     }
+// }
+
+// let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+// let caffinated = false;
+
+// for (let i = 0; i < groceryList.length; i++){
+//     console.log(`Purchasing ${groceryList[i]}`);
+//     if(groceryList[i] === 'coffee beans'){
+//         console.log('Time to brew!!!');
+//         caffinated = true;
+//     }
+//     if(caffinated === true){
+//         console.log('More COFEEEE');
+//     }   
+// }
+// console.log('Finished buying my shit.');
