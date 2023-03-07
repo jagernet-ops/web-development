5ca494e9ad44db8c51db72df737eb7a5ff0593ea
jagernet-ops <mshalewski@tutanota.com>
2023-03-06 22:34:19 -0600

    finished beginner javascript course and added certification to folder alongside work

--- Codecademy Projects/advanced objects/index.js
+++ Codecademy Projects/advanced objects/index.js
@@ -103,3 +103,57 @@
 // robot.numOfSensors = 100;
 // console.log(robot.numOfSensors);
 
+// const robotFactory = (model, mobile) => {
+//   return {
+//     model,
+//     mobile,
+//     beep() {
+//       console.log('Beep Boop');
+//     }
+//   }
+// }
+
+// // To check that the property value shorthand technique worked:
+// const newRobot = robotFactory('P-501', false)
+// console.log(newRobot.model)
+// console.log(newRobot.mobile)
+
+// const robot = {
+//   model: '1E78V2',
+//   energyLevel: 100,
+//   functionality: {
+//     beep() {
+//       console.log('Beep Boop');
+//     },
+//     fireLaser() {
+//       console.log('Pew Pew');
+//     },
+//   }
+// };
+
+// const { functionality } = robot;
+// functionality.beep();
+
+// const robot = {
+// 	model: 'SAL-1000',
+//   mobile: true,
+//   sentient: false,
+//   armor: 'Steel-plated',
+//   energyLevel: 75
+// };
+
+// // What is missing in the following method call?
+// const robotKeys = Object.keys(robot);
+
+// console.log(robotKeys);
+
+// // Declare robotEntries below this line:
+// const robotEntries = Object.entries(robot);
+
+
+// console.log(robotEntries);
+
+// // Declare newRobot below this line:
+// const newRobot = Object.assign({laserBlaster: true, voiceRecognition: true}, robot);
+
+// console.log(newRobot);
