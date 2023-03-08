af8c4f6b682ec1e89b9e2b35c0af98558efb1ffc
jagernet-ops <mshalewski@tutanota.com>
2023-03-08 16:56:54 -0600

    Created folders to subsect web development into HTML, CSS, and Javascript

--- Learning-Javascript/logging-to-the-console/index.js
+++
@@ -1,8 +0,0 @@
-/*
-* The console log function allows us to debug code through
-* printing the output of functions to the browser's console.
-* This function reminds me heavily of python's:print(), java's
-* System.out.println(), and even of C++'s: std::cout
-*/
-console.log("Black is the best kind of coffee");
-console.log("Creamer is for weaklings who like vim")
